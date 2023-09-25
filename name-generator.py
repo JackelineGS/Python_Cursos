b07a1cb0d3f1250f64919bee30ccb6b4226d07e6
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-09-25 17:10:43 -0500

    Day One

---
+++ name-generator.py
@@ -0,0 +1,18 @@
+# Lesson: Printing 
+print('Hello Word')
+# Exercise 
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# String Manipulation and Code Intelligence
+
+print("Day 1 - Python Print Function \n The function is declared like this: \n print('what to print') ")
+
+print('Hello' + ' Angella')
+print('Hello' + ' ' + 'Angella')
+ 
+#Nota: En python tengamos cuidado con la identación. Hay errores de sintaxis y de identación
+
+# The Python Input Function
+

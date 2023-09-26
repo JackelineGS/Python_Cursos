176ce34f81d4f8bb507450d27453b63437d2ad09
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-09-26 17:33:35 -0500

    We learned about input

--- name-generator.py
+++ name-generator.py
@@ -15,4 +15,9 @@ print('Hello' + ' ' + 'Angella')
 #Nota: En python tengamos cuidado con la identación. Hay errores de sintaxis y de identación
 
 # The Python Input Function
+print('¿What is your name?')
+input('¿What is your name?')
+print('Hello ' + input('What is your name?'))
 
+# Excersise number 1.3
+print(len(input('What is your name? ')))

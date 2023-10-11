b26bdb1b3c8bbf5d1723a341db4424acc706607c
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-10-10 23:15:46 -0500

    We add excercises

---
+++ excerciases.py
@@ -0,0 +1,33 @@
+# CLASE 2 
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+a = float(weight)
+b = float(height)
+
+bmi = a/(b*b)
+bmi = int(bmi)
+print(bmi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
